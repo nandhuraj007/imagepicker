75ebbc0af1a54a0bfeb3529e8bdc4ec610984bf5
Nandhu Raj <112924887+nandhuraj007@users.noreply.github.com>
2023-01-17 21:18:15 +0530

    Add files via upload

---
+++ select_photo_options_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '/widgets/re_usable_select_photo_button.dart';
+
+class SelectPhotoOptionsScreen extends StatelessWidget {
+  const SelectPhotoOptionsScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.grey.shade300,
+      padding: const EdgeInsets.all(20),
+      child: Stack(
+        alignment: AlignmentDirectional.topCenter,
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(
+            top: -35,
+            child: Container(
+              width: 50,
+              height: 6,
+              margin: const EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(2.5),
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Column(children: [
+            SelectPhoto(
+              onTap: () {},
+              icon: Icons.image,
+              textLabel: 'Browse Gallery',
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Center(
+              child: Text(
+                'OR',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SelectPhoto(
+              onTap: () {},
+              icon: Icons.camera_alt_outlined,
+              textLabel: 'Use a Camera',
+            ),
+          ])
+        ],
+      ),
+    );
+  }
+}
